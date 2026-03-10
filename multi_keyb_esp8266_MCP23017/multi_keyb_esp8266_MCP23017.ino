348892f219dd1dbe2b5b165316d8eedefd0b6289
Teemu Nousiainen <tnousiai@Teemus-MacBook-Air.local>
2026-03-10 15:59:19 +0200

    Multi-keyboard

--- multi_keyb_esp8266_MCP23017/multi_keyb_esp8266_MCP23017.ino
+++ multi_keyb_esp8266_MCP23017/multi_keyb_esp8266_MCP23017.ino
@@ -13,9 +13,10 @@ MCP23017 mcp1(I2C_SDA, I2C_SCL);  //create mcp instance
 #define MIDI_NOTE_C2 36
 
 int value, b, i, j, key, note;
+int debug_mode = 1;
+unsigned long led_ts = 0;
 
 void noteOn(int cmd, int pitch, int velocity, int i, int j);
-
 
 class MCP23017Keyboard
 {
@@ -69,22 +70,24 @@ void MCP23017Keyboard::Init(void)
       this->output_pins[-this->pin_map[i] - 1] = i;
   }
 
-  Serial.print("Note map: ");
-  for (i = 0; i < this->keys; i++) 
+  if (debug_mode)
   {
-    Serial.print(this->note_map[i]);
-    Serial.print(" ");
-  }
-  Serial.println();
+    Serial.print("Note map: ");
+    for (i = 0; i < this->keys; i++) 
+    {
+      Serial.print(this->note_map[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
 
-  Serial.print("Output pins: ");
-  for (i = 0; i < this->octaves; i++) 
-  {
-    Serial.print(this->output_pins[i]);
-    Serial.print(" ");
+    Serial.print("Output pins: ");
+    for (i = 0; i < this->octaves; i++) 
+    {
+      Serial.print(this->output_pins[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
   }
-  Serial.println();
-
 
   for (j = 0; j < 4; j++) 
   {
@@ -100,11 +103,14 @@ void MCP23017Keyboard::Init(void)
   {
     if (this->pin_map[i] >= 0)
       input = input | (1 << i);
-    else
-      this->output_pins[1 - pin_map[i]] = i;
   }
-  Serial.print("PORT A Input mask: ");
-  Serial.println(input, BIN);
+
+  if (debug_mode)
+  {
+    Serial.print("PORT A Input mask: ");
+    Serial.println(input, BIN);
+  }
+
   this->mcp->iodir(MCP23017_PORTA, input, this->address);
 
   input = 0;
@@ -112,23 +118,25 @@ void MCP23017Keyboard::Init(void)
   {
     if (this->pin_map[i + 8] >= 0)
       input = input | (1 << i);
-    else
-      this->output_pins[-pin_map[i + 8]] = i + 8;
   }
-  Serial.print("PORT B Input mask: ");
-  Serial.println(input, BIN);
+
+  if (debug_mode)
+  {
+    Serial.print("PORT B Input mask: ");
+    Serial.println(input, BIN);
+  }
   this->mcp->iodir(MCP23017_PORTB, input, address);
   this->mcp->write_gpio(MCP23017_PORTA, 0x00, this->address);
   this->mcp->write_gpio(MCP23017_PORTB, 0x00, this->address);
 }
 
 int time_threshold = 2000;
-// int keyb_map[12] = {12,11,10,9,7,8,6,5,1,2,3,4};
 
 void MCP23017Keyboard::poll_status(void)
 {
   int i, j, mask, key_state, value, note;
   unsigned long current_ts;
+
 
   for (int j = 0; j < this->octaves; j++)
   {
@@ -137,11 +145,9 @@ void MCP23017Keyboard::poll_status(void)
     else
       mcp1.write_gpio(MCP23017_PORTB, 0x1 << (this->output_pins[j] - 8), this->address);
     delay(10);
-//    Serial.println(0x1 << this->output_pins[j]);
 
 
     value = mcp1.read_gpio(MCP23017_PORTA, this->address) | (mcp1.read_gpio(MCP23017_PORTB, this->address) << 8);
-//    Serial.println(value);
 
     for (int i = 0; i < this->keys; i++) {
       mask = 1 << this->note_map[i];
@@ -169,40 +175,80 @@ void MCP23017Keyboard::poll_status(void)
 
 void noteOn(int cmd, int pitch, int velocity, int i, int j) 
 {
-  if (velocity == 0) 
+  digitalWrite(LED_BUILTIN, LOW); // Turn on LED
+  led_ts = micros();
+
+  if (debug_mode)
   {
-    Serial.print("Note ");
-    Serial.print(pitch);
-    Serial.print(" off");
-    Serial.println("");
+    if (velocity == 0) 
+    {
+      Serial.print("Note ");
+      Serial.print(pitch);
+      Serial.print(" off");
+      Serial.println("");
+    }
+    else 
+    {
+      Serial.print("Note ");
+      Serial.print(pitch);
+      Serial.print(" on ");
+      Serial.print(" ");
+      Serial.print(i);
+      Serial.print(" ");
+      Serial.print(j);
+      Serial.println("");
+    } 
   }
-  else 
+  else
   {
-    Serial.print("Note ");
-    Serial.print(pitch);
-    Serial.print(" on ");
-    Serial.print(" ");
-    Serial.print(i);
-    Serial.print(" ");
-    Serial.print(j);
-    Serial.println("");
+    Serial.write(cmd);
+    Serial.write(pitch);
+    Serial.write(velocity);
   }
-/*  Serial.write(cmd);
-  Serial.write(pitch);
-  Serial.write(velocity); */
 }
 
 MCP23017Keyboard *pedals;
+MCP23017Keyboard *lower_keyb;
+MCP23017Keyboard *upper_keyb;
+
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
   Serial.begin(74880);
+
   int pedal_pinmap[] = {-1, 999, 999, 0, 1, 2, 3, 4,   12,11,10,9,8,7,6,5};
-  pedals = new MCP23017Keyboard(&mcp1, MCP23017_ADDRESS_22, 1, 13, MIDI_NOTE_C2, pedal_pinmap, 0);
-  Serial.println("Initializing");
+  pedals = new MCP23017Keyboard(&mcp1, MCP23017_ADDRESS_22, 1, 13, MIDI_NOTE_C2, pedal_pinmap, 2);  
+  if (debug_mode)
+    Serial.println("Initializing Pedals");
   pedals->Init();
-  Serial.println("OK!");
+  if (debug_mode)
+    Serial.println("OK!");
+
+  int lower_pinmap[] = {-4, -3, -2, -1, 11,10,9,7,   0, 1, 2, 3, 4,5,6,8};
+  lower_keyb = new MCP23017Keyboard(&mcp1, MCP23017_ADDRESS_21, 4, 12, MIDI_NOTE_F3, lower_pinmap, 1);
+  if (debug_mode)
+    Serial.println("Initializing Lower Keyboard");
+  lower_keyb->Init();
+  if (debug_mode)
+    Serial.println("OK!");
+
+  int upper_pinmap[] = {-4, -3, -2, -1, 11,10,9,8,   0, 1, 2, 3, 4,5,6,7};
+  upper_keyb = new MCP23017Keyboard(&mcp1, MCP23017_ADDRESS_20, 4, 12, MIDI_NOTE_F3, upper_pinmap, 0);
+  if (debug_mode)
+    Serial.println("Initializing Upper Keyboard");
+  upper_keyb->Init();
+  if (debug_mode)
+    Serial.println("OK!");
 }
 
 void loop() {
+  if ((led_ts != 0) && (micros() - led_ts > 30000))
+  {
+    digitalWrite(LED_BUILTIN, HIGH); // Turn off LED
+    led_ts = 0;
+  }
   pedals->poll_status();
+  lower_keyb->poll_status();
+  upper_keyb->poll_status();
 }
